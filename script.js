d250903059e03bd84d735ee5ba93123488f7ab33
brunoafigueira <brunoafigueira@gmail.com>
2026-03-12 15:35:07 -0300

    Replace string feeds with object array for news sourcesmorenews

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const feeds = [
-"https://g1.globo.com/rss/g1/",
-"https://feeds.folha.uol.com.br/emcimadahora/rss091.xml",
-"https://rss.estadao.com.br/feed",
-"https://www.cnnbrasil.com.br/rss",
-"https://www.poder360.com.br/feed/"
+
+{nome:"G1 Economia", url:"https://g1.globo.com/rss/g1/economia/"},
+{nome:"Valor Econômico", url:"https://valor.globo.com/rss/"},
+{nome:"InfoMoney", url:"https://www.infomoney.com.br/feed/"},
+{nome:"Exame Economia", url:"https://exame.com/feed/"},
+{nome:"CNN Brasil Economia", url:"https://www.cnnbrasil.com.br/economia/feed/"},
+{nome:"Estadão Economia", url:"https://rss.estadao.com.br/economia.xml"},
+
+{nome:"ConJur", url:"https://www.conjur.com.br/rss.xml"},
+{nome:"Consultor Jurídico", url:"https://www.conjur.com.br/rss.xml"},
+{nome:"Migalhas", url:"https://www.migalhas.com.br/rss"},
+{nome:"Jota", url:"https://www.jota.info/feed"},
+{nome:"Justificando", url:"https://www.justificando.com/feed/"},
+
+{nome:"STF Notícias", url:"https://www.stf.jus.br/portal/rss/noticiaRss.asp"},
+{nome:"STJ Notícias", url:"https://www.stj.jus.br/sites/portalp/rss/noticias.xml"},
+{nome:"CNJ Notícias", url:"https://www.cnj.jus.br/feed/"},
+{nome:"TST Notícias", url:"https://www.tst.jus.br/rss/-/asset_publisher/89Dk/rss"},
+
+{nome:"AB2L Legaltech", url:"https://www.ab2l.org.br/feed/"},
+{nome:"Artificial Lawyer", url:"https://www.artificiallawyer.com/feed/"},
+{nome:"LawSites", url:"https://www.lawsitesblog.com/feed"},
+{nome:"Legal IT Insider", url:"https://legaltechnology.com/feed/"},
+
+{nome:"Serasa Experian", url:"https://www.serasaexperian.com.br/conteudos/feed/"},
+{nome:"SPC Brasil", url:"https://www.spcbrasil.org.br/blog/feed/"},
+{nome:"Proteste", url:"https://www.proteste.org.br/rss"},
+{nome:"Banco Central Notícias", url:"https://www.bcb.gov.br/rss/noticias"},
+
+{nome:"Tecnoblog", url:"https://tecnoblog.net/feed/"},
+{nome:"Olhar Digital", url:"https://olhardigital.com.br/feed/"},
+{nome:"MIT Technology Review BR", url:"https://mittechreview.com.br/feed/"},
+{nome:"VentureBeat AI", url:"https://venturebeat.com/category/ai/feed/"}
+
 ];
 
 async function loadNews(){
